897b030fdad6255c1ba4bca8dfe109f92d6f7a22
sbergere <90045192+sbergere@users.noreply.github.com>
2025-03-03 23:55:11 +0900

    Add oil, fix font

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -3,6 +3,8 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
+      local icons = require 'mini.icons'
+      icons.setup { style = 'glyph' }
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = true }
     end

c4f93de0b431551a8f8a855081b10a23f957523d
sbergere <90045192+sbergere@users.noreply.github.com>
2025-03-03 19:35:49 +0900

    Fix shortcut being assigned twice to find grep

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -22,7 +22,6 @@ return {
 
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<space>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<space>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<space>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<space>fh', builtin.help_tags, { desc = 'Telescope help tags' })
       vim.keymap.set('n', '<space>en', function()
